ae58463ad7af5f7fad14a4e93611e328fee7f92d
adoronin158@gmail.com <adoronin158@gmail.com>
2021-10-08 18:53:37 +0300

    1234

---
+++ Chat/Chat/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat
+{
+    
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataBank.Text = listBox1.Text;
+
+            Form1 form = new Form1();
+            form.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             listBox1.Items.Add(richTextBox1.Text);
+        }
+    }
+}
